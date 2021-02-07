4ef0e4210d2325852c48e82af2187588de19757b
rahul chaudhuri <rahul@paul.com>
2021-02-07 09:43:51 +0000

    learnwithproject.com project

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+include_recipe 'learnwithproject.com::misl'
+include_recipe 'learnwithproject.com::apache'
+include_recipe 'learnwithproject.com::tomcat'
+
